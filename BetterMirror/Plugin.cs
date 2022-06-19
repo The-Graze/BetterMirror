646f5ee73d4eafa751591eab49ef8e07de7f2293
Graze <reece2004f@gmail.com>
2022-06-19 09:19:58 +0100

    Add project files.

---
+++ BetterMirror/Plugin.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using Utilla;
+
+namespace BetterMirror
+{
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.6.4")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool inRoom;
+        public GameObject bm;
+
+        void OnEnable()
+        {
+            /* Set up your mod here */
+            /* Code here runs at the start and whenever your mod is enabled*/
+
+            HarmonyPatches.ApplyHarmonyPatches();
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+        void OnDisable()
+        {
+            HarmonyPatches.RemoveHarmonyPatches();
+            Utilla.Events.GameInitialized -= OnGameInitialized;
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            Stream str = Assembly.GetExecutingAssembly().GetManifestResourceStream("BetterMirror.Assets.bm");
+            AssetBundle bundle = AssetBundle.LoadFromStream(str);
+            GameObject mirror = Instantiate(bundle.LoadAsset<GameObject>("bm"));
+            GameObject.Find("Level/city/CosmeticsRoomAnchor/ShoppingCenterAnchor/mirrors2 (1)/ShoppingCart/").transform.SetParent(GameObject.Find("bm(Clone)").transform, false);
+            Destroy(GameObject.Find("Level/city/CosmeticsRoomAnchor/ShoppingCenterAnchor/mirrors2 (1)/"));
+            GameObject.Find("bm(Clone)").transform.SetParent(GameObject.Find("Level/city/CosmeticsRoomAnchor/").transform, true);
+        }
+
+    }
+
+
+}
---
+++ BetterMirror/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace BetterMirror
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.graze.gorillatag.bettermirror";
+        public const string Name = "BetterMirror";
+        public const string Version = "1.0.0";
+    }
+}
