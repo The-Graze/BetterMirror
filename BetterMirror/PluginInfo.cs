a4362126b0ca7e36f09e31ce1679166771e17082
The-Graze <reece2004f@gmail.com>
2024-03-27 14:39:17 +0000

    allways forgot plugin info lol

--- BetterMirror/PluginInfo.cs
+++ BetterMirror/PluginInfo.cs
@@ -1,12 +1,9 @@
 ﻿namespace BetterMirror
 {
-    /// <summary>
-    /// This class is used to provide information about your mod to BepInEx.
-    /// </summary>
     internal class PluginInfo
     {
-        public const string GUID = "com.graze.gorillatag.bettermirror";
+        public const string GUID = "graze.bettermirror";
         public const string Name = "BetterMirror";
-        public const string Version = "2.2";
+        public const string Version = "2.3";
     }
 }
