e06e51d8f6b2c4312e07320f26624e69c61b355c
The-Graze <reece2004f@gmail.com>
2024-03-27 14:32:33 +0000

    fix

--- BetterMirror/Plugin.cs
+++ BetterMirror/Plugin.cs
@@ -8,55 +8,43 @@ using Oculus.Platform;
 namespace BetterMirror
 {
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
-    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInDependency("org.legoandmars.gorillatag.utilla")]
     public class Plugin : BaseUnityPlugin
     {
         public RenderTexture bm;
-        public static volatile Plugin Instance;
-        public int cull;
+        bool ran;
         void Start()
         {
             Utilla.Events.GameInitialized += OnGameInitialized;
         }
         void OnGameInitialized(object sender, EventArgs e)
         {
-            Instance = this;
             bm = new RenderTexture(2000, 2000, 24, RenderTextureFormat.Default);
-            bm.Create();
             bm.filterMode = FilterMode.Point;
             bm.antiAliasing = 3;
+            bm.Create();
             GameObject.Find("LocalObjects_Prefab").AddComponent<LayerChanger>();
-            gameObject.AddComponent<CamChange>();
-        }
-        void Update()
-        {
-            if(cull == 0) 
-            {
-                GameObject.Find("Environment Objects/LocalObjects_Prefab").transform.GetChild(3).gameObject.SetActive(true);
 
-                GameObject.Find("mirrors2 (1)").GetComponent<Renderer>().materials[1].mainTexture = bm;
-                GameObject.Find("CameraC").GetComponent<Camera>().targetTexture = bm;
-            }
-            else
-            {
-                gameObject.GetComponent<CamChange>().cull = cull;
-                Destroy(this);
-            }
+            int mirrorOnlyMask = 1 << LayerMask.NameToLayer("MirrorOnly");
+            int noMirrorMask = 1 << LayerMask.NameToLayer("NoMirror");
+
+            Camera.allCameras[1].cullingMask |= mirrorOnlyMask;
+            Camera.allCameras[1].cullingMask &= ~noMirrorMask;
         }
-    }
-    class CamChange : MonoBehaviour
-    {
-        public int cull;
         void Update()
         {
-            if (cull != 0)
+            if (ran == false)
             {
-                foreach (Camera c in Camera.allCameras)
+                try
                 {
-                    if (c != Camera.main && c.cullingMask != cull)
-                    {
-                        c.cullingMask = cull;
-                    }
+                    GameObject.Find("mirrors2 (1)").GetComponent<Renderer>().materials[1].mainTexture = bm;
+                    GameObject.Find("CameraC").GetComponent<Camera>().targetTexture = bm;
+                    GameObject.Find("Environment Objects/LocalObjects_Prefab/mirrors2 (1)").transform.GetChild(3).gameObject.SetActive(false);
+                    ran = true;
+                }
+                catch
+                {
+                    GameObject.Find("Environment Objects/LocalObjects_Prefab/mirrors2 (1)").transform.GetChild(3).gameObject.SetActive(true);
                 }
             }
         }
@@ -69,7 +57,6 @@ namespace BetterMirror
             if (gameObject.layer == LayerMask.NameToLayer("NoMirror"))
             {
                 gameObject.layer = 0;
-                Destroy(this);
             }
             if (gameObject.name == "Mirror Backdrop")
             {
@@ -77,14 +64,11 @@ namespace BetterMirror
             }
             if (gameObject.name == "CameraC")
             {
-                Plugin.Instance.cull = gameObject.GetComponent<Camera>().cullingMask;
                 GetComponent<Camera>().farClipPlane = 35;
-                Destroy(this);
             }
             foreach (Transform t in gameObject.transform)
             {
                 t.gameObject.AddComponent<LayerChanger>();
-                Destroy(this);
             }
             Destroy(this);
         }
