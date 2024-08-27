412606578b1dc1257264c1e2af9b46a594f07739
graze <graze>
2024-08-27 21:10:04 +0100

    Fixed For Latest

--- BetterMirror/Plugin.cs
+++ BetterMirror/Plugin.cs
@@ -1,76 +1,29 @@
 ﻿using BepInEx;
-using System;
 using UnityEngine;
-using Utilla;
-using System.Collections;
-using Oculus.Platform;
 
 namespace BetterMirror
 {
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
-    [BepInDependency("org.legoandmars.gorillatag.utilla")]
     public class Plugin : BaseUnityPlugin
     {
-        public RenderTexture bm;
         bool ran;
+        Camera MirrorCam;
+        GameObject Mirror;
         void Start()
         {
-            Utilla.Events.GameInitialized += OnGameInitialized;
+            GorillaTagger.OnPlayerSpawned(OnGameInitialized);
         }
-        void OnGameInitialized(object sender, EventArgs e)
+        void OnGameInitialized()
         {
-            bm = new RenderTexture(2000, 2000, 24, RenderTextureFormat.Default);
-            bm.filterMode = FilterMode.Point;
-            bm.antiAliasing = 3;
-            bm.Create();
-            GameObject.Find("Environment Objects/LocalObjects_Prefab").transform.GetChild(3).gameObject.AddComponent<LayerChanger>();
+            GameObject city = GameObject.Find("Environment Objects/LocalObjects_Prefab").transform.FindChildRecursive("City_WorkingPrefab").gameObject;
+            Mirror = city.transform.FindChildRecursive("DressingRoom_Mirrors_Prefab").gameObject;
 
-            int mirrorOnlyMask = 1 << LayerMask.NameToLayer("MirrorOnly");
-            int noMirrorMask = 1 << LayerMask.NameToLayer("NoMirror");
-
-            Camera.allCameras[1].cullingMask |= mirrorOnlyMask;
-            Camera.allCameras[1].cullingMask &= ~noMirrorMask;
-        }
-        void Update()
-        {
-            if (ran == false)
-            {
-                try
-                {
-                    GameObject.Find("mirrors2 (1)").GetComponent<Renderer>().materials[1].mainTexture = bm;
-                    GameObject.Find("CameraC").GetComponent<Camera>().targetTexture = bm;
-                    GameObject.Find("Environment Objects/LocalObjects_Prefab").transform.GetChild(3).gameObject.SetActive(false);
-                    ran = true;
-                }
-                catch
-                {
-                    GameObject.Find("Environment Objects/LocalObjects_Prefab").transform.GetChild(3).gameObject.SetActive(true);
-                }
-            }
-        }
-    }
-
-    public class LayerChanger : MonoBehaviour
-    {
-        void Start()
-        {
-            if (gameObject.layer == LayerMask.NameToLayer("NoMirror"))
-            {
-                gameObject.layer = 0;
-            }
-            else if (gameObject.name == "Mirror Backdrop")
-            {
-                Destroy(gameObject);
-            }
-            else if (gameObject.name == "CameraC")
-            {
-                GetComponent<Camera>().farClipPlane = 35;
-            }
-            foreach (Transform t in transform)
-            {
-                t.gameObject.AddComponent<LayerChanger>();
-            }
-            Destroy(this);
+            Mirror.transform.GetChild(1).gameObject.SetActive(false);
+            MirrorCam = Mirror.transform.GetComponentInChildren<Camera>();
+            MirrorCam.farClipPlane = 35;
+            MirrorCam.targetTexture.filterMode = FilterMode.Point;
+            MirrorCam.targetTexture.width = MirrorCam.targetTexture.width * 10;
+            MirrorCam.targetTexture.height = MirrorCam.targetTexture.height * 10;
         }
     }
 }
