87c23724bd100f414da3d49be0d73ee8cedc7d12
Graze <Graze>
2024-10-21 22:01:31 +0100

    wawa

--- BetterMirror/PluginInfo.cs
+++ BetterMirror/PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string GUID = "graze.bettermirror";
         public const string Name = "BetterMirror";
-        public const string Version = "3.0";
+        public const string Version = "3.1";
     }
 }
