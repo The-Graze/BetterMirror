43c1c59350bd6c70341e1d4b6b1b21d7eed6be19
graze <me@graze.cc>
2025-12-12 21:23:26 +0000

    Update

--- BetterMirror/Plugin.cs
+++ BetterMirror/Plugin.cs
@@ -1,36 +1,41 @@
 ﻿using System.Threading.Tasks;
 using BepInEx;
 using System.Collections;
+using BepInEx.Configuration;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace BetterMirror
 {
-    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    [BepInPlugin(PluginInfo.Guid, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
-        bool ran;
-        Camera MirrorCam;
-        Transform Mirror;
-        void Start()
-        {
-            GorillaTagger.OnPlayerSpawned(OnGameInitialized);
-        }
-        void OnGameInitialized()
-        {
-            Transform city = GameObject.Find("Environment Objects/LocalObjects_Prefab").transform.FindChildRecursive("City_WorkingPrefab");
-            Mirror = city.FindChildRecursive("DressingRoom_Mirrors_Prefab");
+        private Camera _mirrorCam;
+        private Transform _mirror;
 
-            Mirror.GetChild(1).gameObject.SetActive(false);
-            MirrorCam = Mirror.GetComponentInChildren<Camera>();
-            MirrorCam.farClipPlane = 35;
-            MirrorCam.targetTexture.filterMode = FilterMode.Point;
-            MirrorCam.targetTexture.width = MirrorCam.targetTexture.width * 5;
-            MirrorCam.targetTexture.height = MirrorCam.targetTexture.height * 5;
+        private static ConfigEntry<int> _quality;
+        
+        private Plugin() =>
+            _quality = Config.Bind("Settings", "Mirror Quality Multiplier", 5, "Times' the mirror quality by this number");
+        
+        private void Start() => SceneManager.sceneLoaded += CityLoadedCheck;
+        
+        private void CityLoadedCheck(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name != "City") return;
+            var city = GameObject.Find("City_Pretty").transform;
+            _mirror = city.FindChildRecursive("DressingRoom_Mirrors_Prefab");
 
+            _mirror.GetChild(1).gameObject.SetActive(false);
+            _mirrorCam = _mirror.GetComponentInChildren<Camera>();
+            _mirrorCam.farClipPlane = 35;
+            _mirrorCam.targetTexture.filterMode = FilterMode.Point;
+            _mirrorCam.targetTexture.width *= _quality.Value;
+            _mirrorCam.targetTexture.height *= _quality.Value;
+            
             SetLayers(city.transform);
         }
-
-        void SetLayers(Transform t)
+        private static void SetLayers(Transform t)
         {
             if (t.gameObject.layer == LayerMask.NameToLayer("NoMirror"))
             {
